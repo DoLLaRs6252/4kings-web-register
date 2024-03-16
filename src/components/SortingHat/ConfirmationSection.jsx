7bf7656bd9f4fc170999b321fab87b60d1e9bb84
DoLLaRs6252 <punvarattaya6252@gmail.com>
2024-03-16 23:25:31 +0700

    responsive

--- src/components/SortingHat/ConfirmationSection.jsx
+++ src/components/SortingHat/ConfirmationSection.jsx
@@ -9,38 +9,37 @@ export default function ConfirmationSection({ count, inputValue, handleInputChan
     };
 
     return (
-        <div className="py-12 w-[50%] h-64 ">
-            <div className=" max-w-3xl  w-[100%] mx-auto px-4 ">
-                <div className="bg-white p-6 rounded-lg shadow-lg items-center space-y-4">
+        <div className="py-12 md:w-1/2 lg:w-1/3 mx-auto">
+            <div className="max-w-lg mx-auto px-4 ">
+                <div className="bg-white p-6  rounded-lg shadow-lg items-center space-y-4">
                     <div>
-                        <div className="text-[8rem] text-center font-bold">{count} Kings</div>
+                        <div className="text-5xl md:text-6xl lg:text-7xl text-center font-bold">{count} Kings</div>
                     </div>
-                    <div className='flex justify-center'>
+                    <div className='flex flex-col md:flex-row items-center justify-center'>
                         <input
                             type="text"
                             value={inputValue}
                             onChange={handleInputChange}
                             placeholder="Number of people"
-                            className="input input-bordered w-[500px] max-w-xs m-4"
+                            className="input input-bordered w-full md:w-[80%] max-w-md md:max-w-none m-4 mb-0"
                             disabled={confirmed}
                         />
-                        <div className="items-center">
+                        
+                    </div>
+                    <div className="flex justify-center items-center ">
                             {!confirmed && (
                                 <button
-                                    className="btn btn-outline btn-error m-4"
+                                    className="btn btn-outline btn-error m-4 md:w-[80%]"
                                     onClick={handleConfirmClick}
                                 >
                                     Confirm
                                 </button>
                             )}
-                            <div className="items-center">
-                                {confirmed && <p className= " text-gray-500 m-7">Confirmed</p>}
+                            <div className="items-center ">
+                                {confirmed && <p className="text-gray-500 m-4">Confirmed</p>}
                             </div>
-                            
                         </div>
-                    </div>
                 </div>
-
             </div>
         </div>
     );
--- src/components/SortingHat/RegistrationSection.jsx
+++ src/components/SortingHat/RegistrationSection.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 export default function RegistrationSection({ remainingApplicants, handleNameSubmit }) {
   return (
-    <div className=" py-12 w-[50%] ">
-      <div className="max-w-3xl  mx-auto px-4">
-        <div className="bg-white p-6 rounded-lg shadow-lg text-center">
-          <h2 className="text-[4rem] font-bold mb-6">Register now!</h2>
+    <div className="py-12 md:w-1/2 lg:w-1/3 mx-auto">
+      <div className="max-w-md md:max-w-3xl mx-auto px-4">
+        <div className="bg-white p-10 rounded-lg shadow-lg text-center">
+          <h2 className="text-3xl md:text-4xl lg:text-5xl font-bold mb-6">Register now!</h2>
           <form onSubmit={handleNameSubmit}>
             <div className="form-control items-center">
               <label className="label">
@@ -15,15 +15,15 @@ export default function RegistrationSection({ remainingApplicants, handleNameSub
                 type="text"
                 name="name"
                 placeholder="Name"
-                className="input input-bordered input-error  w-[80%] text-red-600"
+                className="input input-bordered input-error w-full md:w-[80%] text-red-600"
                 required
                 disabled={remainingApplicants === 0}
               />
             </div>
-            <div className="form-control mt-6 items-center ">
+            <div className="form-control mt-6 flex justify-center items-center">
               <button
                 type="submit"
-                className="btn btn-outline btn-error w-[80%] "
+                className="btn btn-outline btn-error w-full md:w-[80%]"
                 disabled={remainingApplicants === 0}
               >
                 Submit
